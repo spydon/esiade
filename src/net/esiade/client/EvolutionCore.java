9d149fe19c50fe873d25a3b98162a6b9ee360ccf
Jonathan Sharyari <sharyari@gmail.com>
2012-05-15 21:50:19 +0200

    Fixed javadoc

--- src/net/esiade/client/EvolutionCore.java
+++ src/net/esiade/client/EvolutionCore.java
@@ -4,26 +4,47 @@ import com.google.gwt.user.client.Random;
 
 import net.esiade.client.sprite.Individual;
 
+/**
+ * @author jonathan
+ *
+ */
 public class EvolutionCore {
-	public static int WIDTH = 20, HEIGHT = 20;
-	private int mRate, cRate;
+	public static int WIDTH, HEIGHT;
+	private static int mRate, cRate;
 	
+	/**
+	 * @param width The width of the map matrix
+	 * @param height The height of the map matrix
+	 * @param mRate The mutation rate
+	 * @param cRate The crossover rate
+	 */
 	public EvolutionCore(int width, int height, int mRate, int cRate){
 		EvolutionCore.WIDTH = width;
 		EvolutionCore.HEIGHT = height;
-		this.mRate = mRate;
-		this.cRate = cRate;
+		EvolutionCore.mRate = mRate;
+		EvolutionCore.cRate = cRate;
 	}
 	
+	/**
+	 * @param width 
+	 * @param height
+	 */
 	public static void setDimensions(int width, int height) {
 		EvolutionCore.WIDTH = width;
 		EvolutionCore.HEIGHT = height;
 	}
 
+	/**
+	 * @author jonathan
+	 * An enumeration of crossover-types. The names are self-explanatory.
+	 */
 	public static enum CType {
-		ONEPOINT, TWOPOINT, UNIFORM, ARITHMETIC
+		ONEPOINT, TWOPOINT, UNIFORM
 	}	
 	
+	/**
+	 * @return This function returns a genome with all vectors set at random.
+	 */
 	public static Vector2D[][] getRandomGenome() {
 		Vector2D[][] genome = new Vector2D[WIDTH][HEIGHT];
 		for (int x = 0;x < WIDTH;x++)
@@ -32,74 +53,101 @@ public class EvolutionCore {
 		return genome;
 	}
 
+	/**
+	 * This function will change the vector with the position (x,y) in the matrix between I1 and I2
+	 * @param I1 The first individual
+	 * @param I2 The second individual
+	 * @param x The x-coordinate in the matrix 
+	 * @param y The y-coordinate in the matrix
+	 * 
+	 */
+	public static void SwitchVectors(Individual I1, Individual I2, int x, int y){
+		Vector2D temp;		
+		temp = I1.genome[x][y];
+		I1.genome[x][y] = I2.genome[x][y];
+		I2.genome[x][y] = temp;
+	}
 	
-	public static Individual Crossover(Individual I1, Individual I2, CType type) {
-		Individual I = null;
+	
+	/**
+	 * This is a general function for crossover, this function redirects to the appropriate crossover subfunction. The subfunctions implement common crossover operations, but applied on vectors instead of bits.
+	 * @param I1 The first individual
+	 * @param I2 The second individual
+	 * @param type The type of crossover to be done, see CType.
+	 */
+	public static void Crossover(Individual I1, Individual I2, CType type) {
+	if (Random.nextDouble() < cRate)
 		if (type == CType.ONEPOINT)
-			I = OnePointCrossover(I1, I2);
+			OnePointCrossover(I1, I2);
 		else if (type == CType.TWOPOINT)
-			I = TwoPointCrossover(I1, I2);
+			TwoPointCrossover(I1, I2);
 		else if (type == CType.UNIFORM)
-			I = UniformCrossover(I1, I2);
-		else if (type == CType.ARITHMETIC)	
-			I = ArithmeticCrossover(I1, I2);
-		
-		return I;
+			UniformCrossover(I1, I2);
+
+		Mutation(I1);
+		Mutation(I2);
 	}
 
-	private static Individual OnePointCrossover(Individual I1, Individual I2){
-		Individual newI = I2;
+	/**
+	 * One point crossover. The two individuals will be modified to two new individuals, in a 1-point crossover fashion. 
+	 * @param I1 The first individual
+	 * @param I2 The second individual
+	 */
+	private static void OnePointCrossover(Individual I1, Individual I2){
+
 		int randomPoint = Random.nextInt(WIDTH*HEIGHT);
 		for(int y = 0;y <= HEIGHT; y++)
 			for (int x = 0; x <= WIDTH; x++)
 				if (randomPoint>0){
-					newI.genome[x][y] = I1.genome[x][y];
+					SwitchVectors(I1, I2, x, y);
 					randomPoint--;
 				} else {	
 					break;
 				}
-		return newI;
 	}
 	
-	private static Individual TwoPointCrossover(Individual I1, Individual I2){
-		Individual newI;
-		newI = I2;
+	/**
+	 * One point crossover. The two individuals will be modified to two new individuals, in a 2-point crossover fashion. 
+	 * @param I1 The first individual
+	 * @param I2 The second individual
+	 */
+	private static void TwoPointCrossover(Individual I1, Individual I2){
 		int randomPoint1 = Random.nextInt(WIDTH*HEIGHT);
 		int randomPoint2 = Random.nextInt(WIDTH*HEIGHT-randomPoint1);
-		randomPoint2 +=randomPoint1;
 		
 		for(int y = randomPoint1 / WIDTH;y <= HEIGHT; y++)
 			for (int x = randomPoint1 % WIDTH; x <= WIDTH; x++)
-				if (randomPoint1 != randomPoint2){
-					newI.genome[x][y] = I1.genome[x][y];
+				if (randomPoint2 >= 0){
+					SwitchVectors(I1, I2, x, y);
 					randomPoint2--;
 				} else {	
 					break;
 				}
-		return newI;
 	}
 	
-	private static Individual UniformCrossover(Individual I1, Individual I2){
-		Individual newI;
-		newI = I2;
+	/**
+	 * One point crossover. The two individuals will be modified to two new individuals, in a uniform crossover fashion. 
+	 * @param I1 The first individual
+	 * @param I2 The second individual
+	 */
+	private static void UniformCrossover(Individual I1, Individual I2){
 		for(int y = 0;y<=HEIGHT;y++)
 			for(int x=0;x<=WIDTH;x++)
-				if (Random.nextDouble() > 0.5)
-					newI.genome[x][y] = I1.genome[x][y];
-
-		return newI;
-
-	}
-	
-	private static Individual ArithmeticCrossover(Individual I1, Individual I2){
-		return null;
+				if (Random.nextDouble() > 0.5){
+					SwitchVectors(I1, I2, x, y);
+				}
 	}
 	
 	
-	private static Individual Mutation(Individual I){
+	
+	/**
+	 * This function will change a vector to a random new vector, with the probability set as mutation rate, see mRate.
+	 * @param I The individual to mutate.
+	 */
+	private static void Mutation(Individual I){
 		for (int y = 0;y < HEIGHT;y++)
 			for (int x=0;x< WIDTH;x++)
-				break;
-	return null;
+				if (Random.nextDouble() < mRate)
+					I.genome[x][y] = new Vector2D();
 	}
 }
