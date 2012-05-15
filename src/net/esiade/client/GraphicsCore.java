ca2d2f79851255b21a29011bcdc92553eebd110a
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2012-05-15 16:17:01 +0200

    Fixing the main core

--- src/net/esiade/client/GraphicsCore.java
+++ src/net/esiade/client/GraphicsCore.java
@@ -24,15 +24,26 @@ import com.google.gwt.canvas.dom.client.CssColor;
 public class GraphicsCore {
 	private Canvas canvas, canvasBuffer;
 	private Context2d context, contextBuffer;
-	public static final int WIDTH = 800, HEIGHT = 800;
+	public static int WIDTH, HEIGHT;
 	private final int REFRESH_RATE = 40;
 	private final CssColor REDRAW_COLOR = CssColor.make("red");
-    private ArrayList<Individual> individuals = new ArrayList<Individual>(0);
-    private ArrayList<Obstacle> obstacles = new ArrayList<Obstacle>(0);
-    private ArrayList<Food> foods = new ArrayList<Food>(0);
+    private ArrayList<Individual> individuals;
+    private ArrayList<Obstacle> obstacles;
+    private ArrayList<Food> foods;
     private CollisionManager collisionManager;
 
-	public GraphicsCore() {
+	public GraphicsCore(int WIDTH, int HEIGHT,
+						ArrayList<Individual> individuals,
+						ArrayList<Obstacle> obstacles,
+						ArrayList<Food> foods,
+						CollisionManager collisionManager) {
+		GraphicsCore.WIDTH = WIDTH;
+		GraphicsCore.HEIGHT = HEIGHT;
+		this.individuals = individuals;
+		this.obstacles = obstacles;
+		this.foods = foods;
+		this.collisionManager = collisionManager;
+		
 		canvas = Canvas.createIfSupported();
 		canvas.setWidth(WIDTH + "px");
 		canvas.setHeight(HEIGHT + "px");
@@ -45,7 +56,6 @@ public class GraphicsCore {
 		canvasBuffer.setHeight(HEIGHT + "px");
 		canvasBuffer.setCoordinateSpaceWidth(WIDTH);
 		canvasBuffer.setCoordinateSpaceHeight(HEIGHT);
-		//canvasBuffer.setVisible(false);
 		
 		context = canvas.getContext2d();
 		context.setFillStyle(REDRAW_COLOR);
@@ -53,17 +63,6 @@ public class GraphicsCore {
 		contextBuffer = canvasBuffer.getContext2d();
 
 		RootPanel.get("canvasholder").add(canvas);
-//		new Individual("http://www.opentk.com/files/ball.png", 
-//				new Vector2D(), new Vector2D());
-		EvolutionCore.setDimensions(WIDTH/20, HEIGHT/20);
-		individuals.add(new Individual(new Vector2D(200,200), new Vector2D(-4,-2), EvolutionCore.getRandomGenome()));
-		individuals.add(new Individual(new Vector2D(200, 300), new Vector2D(3, 3), EvolutionCore.getRandomGenome()));
-		individuals.add(new Individual(new Vector2D(300, 300), new Vector2D(-2, 0), EvolutionCore.getRandomGenome()));
-		
-		foods.add(new Food(new Vector2D(100,100)));
-		collisionManager = new CollisionManager(WIDTH, HEIGHT, individuals, obstacles, foods);
-		//doUpdate();
-		//context.drawImage((ImageElement)new Image("http://www.opentk.com/files/ball.png").getElement().cast(), 200, 200);
 		RootPanel.get().add(new Label("2"));
 		onModuleLoad();
 
@@ -80,9 +79,6 @@ public class GraphicsCore {
 	}
 	
 	private void doUpdate() {
-		//if(updateCounter == 0)
-			//collisionManager.checkCollision();
-		//updateCounter = (updateCounter+1)%10;
 		contextBuffer.setFillStyle(CssColor.make("GREEN"));
 		contextBuffer.fillRect(0, 0, WIDTH, HEIGHT);
 		collisionManager.checkCollision();
@@ -94,6 +90,9 @@ public class GraphicsCore {
 		
 		for(Food food : foods)
 			food.draw(contextBuffer);
+		
+		for(Obstacle o : obstacles)
+			o.draw(contextBuffer);
 		
 	    context.drawImage(contextBuffer.getCanvas(), 0, 0);
 	}
